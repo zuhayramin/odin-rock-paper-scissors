c11eb735b7757510f9fc65fb038e3b37442d4c58
Zuhayr Amin <44747492+zuhayramin@users.noreply.github.com>
2023-09-13 17:18:32 +0400

    Completed the command line version of the game

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # odin-rock-paper-scissors
+
+A command line version of the classic Rock-Paper-Scissors game
+
+Simply enter either 'rock', 'paper', or 'scissors' when prompted.
+
+The computer will make a random choice, and the result will be displayed in the console.
---
+++ script.js
@@ -0,0 +1,47 @@
+// get the computer to make a choice
+function getComputerChoice() {
+    const computerChoice = Math.floor(Math.random() * 3) + 1
+    if (computerChoice === 1) {
+        return "rock"
+    } else if (computerChoice === 2) {
+        return "paper"
+    } else if (computerChoice === 3) {
+        return "scissors"
+    }
+}
+
+const playerSelection = prompt("Pick rock, paper or scissors: ").toLowerCase()
+const computerSelection = getComputerChoice()
+console.log(playRound(playerSelection, computerSelection))
+
+// // compare the choices and decide who won
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock") {
+        if (computerSelection === "scissors") {
+            return "You Won! Rock beats Scissors"
+        } else if (computerSelection === "paper") {
+            return "You Lost! Paper beats Rock"
+        } else {
+            return "It's a tie!"
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            return "You Won! Paper beats Rock"
+        } else if (computerSelection === "scissors") {
+            return "You Lost! Scissors beats Paper"
+        } else {
+            return "It's a tie!"
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "paper") {
+            return "You Won! Scissors beats Paper"
+        } else if (computerSelection === "rock") {
+            return "You Lost! Rock beats scissors"
+        } else {
+            return "It's a tie!"
+        }
+    } else {
+        return "Please enter a valid input!"
+    }
+}
